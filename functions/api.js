584fe014cc6131c58176ac31d94d9011a4182b76
bhadri2002 <bhadri2002@gmail.com>
2024-03-19 03:40:34 +0530

    path updated

--- functions/api.js
+++ functions/api.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => res.send("Welcome to the root"));
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 // Use the router on a specific path. If you want this to be the root, adjust accordingly.
-app.use('/.netlify/functions/api', router);
+app.use('/', router);
 
 // Export the serverless function handler
 module.exports.handler = serverless(app);
